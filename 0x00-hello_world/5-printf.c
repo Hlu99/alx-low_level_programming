f5274d8cac4152fd797d69390d65e7f0c0dea218
Hlu99 <Hluvuko99@gmail.com>
2022-12-08 15:56:48 -0800

    lal

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,14 @@
-nclude <stdio.h>
-
-
-
+#include <stdio.h>
 /**
+ * Description: main - prints out a string to stdout.
  *
- *  * Description: main - prints out a string to stdout.
+ * Return: 0 if success.
  *
- *   * Return: 0 if success.
- *
- *    */
+ */
 
 int main(void)
 
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
