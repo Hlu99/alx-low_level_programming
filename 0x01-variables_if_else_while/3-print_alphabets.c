1f78689d0487e717e64ac3e97373333664dbaf0c
Hlu99 <Hluvuko99@gmail.com>
2022-12-09 14:48:31 -0800

    haaaa

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,12 +9,19 @@ int main(void)
 {
 	char c;
 
+	char d;
+
 	c = 'a';
+	d = 'A'
 	while
 		(c <= 'z') {
 			putchar(c);
 			c++;
 		}
+	while
+		(d <= 'z') {
+			putchar(c);
+			d++;
 	putchar('\n');
 	return(0);
 }
